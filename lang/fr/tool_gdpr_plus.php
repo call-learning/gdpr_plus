a89142903699cc23fa91dbdd7aa67a90102f33a8
Laurent David <lmedavid@gmail.com>
2022-01-04 12:26:38 +0100

    Fix language for showbanner (in French)

--- lang/fr/tool_gdpr_plus.php
+++ lang/fr/tool_gdpr_plus.php
@@ -34,4 +34,4 @@ $string['policiesconsent:button:rejectnonessential'] = 'Rejeter les non-essentie
 $string['policiesconsent:button:acceptall'] = 'Tout accepter';
 $string['policiesconsent:button:hidesettings'] = 'Cacher';
 $string['policiesconsent:button:acceptselected'] = 'Sauvegarder mes choix';
-$string['policiesconsent:button:showbanner'] = 'Bannière des politiques';
+$string['policiesconsent:button:showbanner'] = 'Mon acceptation des politiques de données';
